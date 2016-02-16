9ea34603c678a07bac176c3f9d3994aeb73c32f4
Jessica Levine <jessica.s.levine@gmail.com>
2016-02-16 19:45:44 +0000

    Return E_NOTIMPL in DLL methods to test building
    In order to test that the build process (particularly as it pertains to
    MIDL compilation) is working, we need to remove the errors relating to no
    return values for DLL methods, which this will temporarily do.

--- PingServer/PingServer.cpp
+++ PingServer/PingServer.cpp
@@ -5,16 +5,17 @@
 #include "PingServer.h"
 
 STDAPI DllCanUnloadNow(void) {
-
+	return E_NOTIMPL;
 }
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv) {
-
+	return E_NOTIMPL;
 }
 
 STDAPI DllRegisterServer(void) {
-
+	return E_NOTIMPL;
 }
 
 STDAPI DllUnregisterServer(void) {
+	return E_NOTIMPL;
 }
