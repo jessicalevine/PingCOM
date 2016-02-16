642ef33e814300ff7019764f8dcb51e9e0d8461e
Jessica Levine <jessica.s.levine@gmail.com>
2016-02-16 19:50:23 +0000

    Migrate MIDL into its own project
    The MIDL definitions, and the proxy/stub DLL code which the MIDL compiler
    outputs, needs to be built in a separate project from the client and
    server. This commit:
    
    * Moves the IDL file into a new PingProxyStubDll project
    * Updates the old ping_h.h links in the client and server to point to this
      new project (including updating the "Additional Include Directories")
    * Adds a module definition file for the PingProxyStubDll
    * Sets up the additional dependencies (rpcrt4.lib) and preprocessor flags
      (_WIN32_WINNT, REGISTER_PROXY_DLL) needed for PingProxyStubDll

--- PingProxyStubDll/ping_h.h
+++ PingProxyStubDll/ping_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Fri Feb 12 21:51:50 2016
+/* at Tue Feb 16 19:41:08 2016
  */
 /* Compiler settings for ping.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
