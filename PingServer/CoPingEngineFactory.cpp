0891fcea70c9e1a409ee2716a9f1af84a56ee04c
Jessica Levine <jessica.s.levine@gmail.com>
2016-02-19 17:59:26 +0000

    Implement QueryInterface for CoPingEngineFactory

--- PingServer/CoPingEngineFactory.cpp
+++ PingServer/CoPingEngineFactory.cpp
@@ -5,7 +5,23 @@
 CoPingEngineFactory::CoPingEngineFactory() { ComponentAddRef(); }
 CoPingEngineFactory::~CoPingEngineFactory() { ComponentRelease(); }
 
-STDMETHODIMP CoPingEngineFactory::QueryInterface(REFIID riid, void **ppv) { return E_NOTIMPL;  }
+STDMETHODIMP CoPingEngineFactory::QueryInterface(REFIID riid, void **ppv) {
+	if (ppv == NULL) {
+		return E_INVALIDARG;
+	}
+
+	if (riid == IID_IUnknown || riid == IID_IClassFactory) {
+		*ppv = static_cast<IClassFactory *>(this);
+	}
+	else {
+		*ppv = NULL;
+		return E_NOINTERFACE;
+	}
+
+	reinterpret_cast<IUnknown *>(ppv)->AddRef();
+
+	return S_OK;
+}
 
 STDMETHODIMP_(ULONG) CoPingEngineFactory::AddRef(void) { return 0; }
 STDMETHODIMP_(ULONG) CoPingEngineFactory::Release(void) { return 0; }
