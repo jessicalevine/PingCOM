c2a721ee075a1663dc65145e120bbe4fd0863c53
Jessica Levine <jessica.s.levine@gmail.com>
2016-02-25 21:19:21 +0000

    Add error checking to PingCli
    DisplayStatus method comes from the example code from the Learning DCOM
    book.

--- PingCli/PingCli.cpp
+++ PingCli/PingCli.cpp
@@ -4,21 +4,60 @@
 #include "stdafx.h"
 #include <iostream>
 
+void DisplayStatus(wchar_t *pwszMsg, HRESULT hr)
+{
+
+	if (hr == S_OK) {
+		wprintf(TEXT("%s\n"), pwszMsg);
+		return;
+	}
+
+	if (HRESULT_FACILITY(hr) == FACILITY_WINDOWS) {
+		hr = HRESULT_CODE(hr);
+	}
+
+	wchar_t *pwszStatus;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		hr,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPWSTR)&pwszStatus,
+		0,
+		NULL);
+
+	wprintf(TEXT("%s: %s (ECode: %lx)\n"), pwszMsg, pwszStatus, hr);
+
+	LocalFree(pwszStatus);
+}
+
 int main() {
-	CoInitializeEx(NULL, COINIT_MULTITHREADED);
-	IPingable * ptrPingable;
-	CoCreateInstance(CLSID_CoPingEngine, NULL, CLSCTX_INPROC, IID_IPingable, reinterpret_cast<void**>(&ptrPingable));
-	ptrPingable->Initialize();
+	HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	if (SUCCEEDED(hr)) {
+		IPingable * ptrPingable;
+		HRESULT hr = CoCreateInstance(CLSID_CoPingEngine, NULL, CLSCTX_LOCAL_SERVER, IID_IPingable, reinterpret_cast<void**>(&ptrPingable));
+		if (SUCCEEDED(hr)) {
+			ptrPingable->Initialize();
 
-	int statusCode;
-	ptrPingable->Ping(12345, &statusCode);
-	std::cout << "Status code for ping code 12345: " << statusCode << std::endl;
-	ptrPingable->Ping(777, &statusCode);
-	std::cout << "Status code for ping code 777: " << statusCode << std::endl;
-	ptrPingable->Ping(1800, &statusCode);
-	std::cout << "Status code for ping code 1800: " << statusCode << std::endl;
+			int statusCode;
+			ptrPingable->Ping(12345, &statusCode);
+			std::cout << "Status code for ping code 12345: " << statusCode << std::endl;
+			ptrPingable->Ping(777, &statusCode);
+			std::cout << "Status code for ping code 777: " << statusCode << std::endl;
+			ptrPingable->Ping(1800, &statusCode);
+			std::cout << "Status code for ping code 1800: " << statusCode << std::endl;
 
-	ptrPingable->Release();
-	CoUninitialize();
-    return 0;
+			ptrPingable->Release();
+		}
+		else {
+			DisplayStatus(TEXT("CoCreateInstance(CoPingEngine) failed: "), hr);
+		}
+		CoUninitialize();
+	}
+	else {
+		DisplayStatus(TEXT("CoInitializeEx failed: "), hr);
+	}
+
+
+	return 0;
 }
